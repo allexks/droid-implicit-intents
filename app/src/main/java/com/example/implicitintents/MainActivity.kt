31ab6412c62b5a33ef64f6d838c102b56cfaadb6
Aleksandar Ignatov <ani1802@abv.bg>
2019-12-06 15:43:55 +0200

    Implement openWebsite

--- app/src/main/java/com/example/implicitintents/MainActivity.kt
+++ app/src/main/java/com/example/implicitintents/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.example.implicitintents
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mWebsiteEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mWebsiteEditText = findViewById(R.id.website_edittext)
+    }
+
+    fun openWebsite(view: View) {
+        val url = mWebsiteEditText.text.toString()
+        val webpage = Uri.parse(url)
+        val intent = Intent(Intent.ACTION_VIEW, webpage)
+
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this intent!")
+        }
     }
 }
