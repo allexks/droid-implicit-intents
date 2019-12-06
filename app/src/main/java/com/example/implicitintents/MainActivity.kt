bab3488032f0ef5c4095bec4d1af70f6435fc977
Aleksandar Ignatov <ani1802@abv.bg>
2019-12-06 15:54:32 +0200

    Implement shareText

--- app/src/main/java/com/example/implicitintents/MainActivity.kt
+++ app/src/main/java/com/example/implicitintents/MainActivity.kt
@@ -7,17 +7,20 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
+import androidx.core.app.ShareCompat
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mWebsiteEditText: EditText
     private lateinit var mLocationEditText: EditText
+    private lateinit var mShareTextEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mWebsiteEditText = findViewById(R.id.website_edittext)
         mLocationEditText = findViewById(R.id.location_edittext)
+        mShareTextEditText = findViewById(R.id.share_edittext)
     }
 
     fun openWebsite(view: View) {
@@ -40,7 +43,18 @@ class MainActivity : AppCompatActivity() {
         if (intent.resolveActivity(packageManager) != null) {
             startActivity(intent)
         } else {
-            Log.d("ImplicitIntents", "Can't open location")
+            Log.d("ImplicitIntents", "Can't open location!")
         }
+    }
+
+    fun shareText(view: View) {
+        val txt = mShareTextEditText.text.toString()
+        val mimeType = "text/plain"
+        ShareCompat.IntentBuilder
+                   .from(this)
+                   .setType(mimeType)
+                   .setChooserTitle(getString(R.string.share_text_with))
+                   .setText(txt)
+                   .startChooser()
     }
 }
